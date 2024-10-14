5e7691588bd207c33a73f16a3636d3952d164549
Dominic <andro5@web.de>
2024-10-14 16:09:05 +0200

    add test without Surefire

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void blubTest(){
+        assertEquals(2, 2);
     }
 
 }
